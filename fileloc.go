31e843c9131fd4546d8fc1b38f69608f83e5d81a
Kosh <kosh@codeworx.com.au>
2018-01-11 17:19:52 +1000

    Initial import

---
+++ fileloc.go
@@ -0,0 +1,77 @@
+//
+// Bet you'll be sick of the word path after reading this documentation.
+//
+
+package fileloc
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+// PathSet represents a set of paths to check for a file
+type PathSet struct {
+	pathSet []string
+}
+
+// New creates an empty PathSet
+func New() *PathSet {
+	p := &PathSet{}
+	return p
+}
+
+// AppendFromPathString appends to the pathset with paths from a path string (colon-separated string)
+func (p *PathSet) AppendFromPathString(pathString string) error {
+	paths := strings.Split(pathString, ":")
+	for _, path := range paths {
+		p.AddPath(path)
+	}
+	return nil
+}
+
+// SetFromPathString sets the pathset with paths from a path string (colon-separated string)
+func (p *PathSet) SetFromPathString(pathString string) error {
+	p.pathSet = []string{}
+	return p.AppendFromPathString(pathString)
+}
+
+// AppendFromEnvironment appends paths from the $PATH environment to the set of paths to search.
+func (p *PathSet) AppendFromEnvironment() error {
+	return p.AppendFromPathString(os.Getenv("PATH"))
+}
+
+// SetFromEnvironment sets the pathset to the same as the $PATH environment
+func (p *PathSet) SetFromEnvironment() error {
+	return p.SetFromPathString(os.Getenv("PATH"))
+}
+
+// AddPath adds a path string to the set of paths to look in.
+func (p *PathSet) AddPath(path string) {
+	p.pathSet = append(p.pathSet, path)
+}
+
+// Find searches for a specified file and returns the absolute path or an error.
+// If the file is already an absolute, it is returned unchanged.
+func (p *PathSet) Find(file string) (string, error) {
+	logrus.Infof("File: %s", file)
+	if strings.HasPrefix(file, "/") {
+		return file, nil
+	}
+	for _, path := range p.pathSet {
+		logrus.Infof("Path: %s", path)
+		fileName := fmt.Sprintf("%s/%s", path, file)
+		logrus.Infof("Filename: %s", fileName)
+		if _, err := os.Stat(fileName); err == nil {
+			absName, err := filepath.Abs(fileName)
+			if err != nil {
+				return fileName, nil
+			}
+			return absName, nil
+		}
+	}
+	return "", fmt.Errorf("File Not Found in Path: %s", file)
+}
